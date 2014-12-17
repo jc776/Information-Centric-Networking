c90c19931c4c7f595061c8a1e3db31b96a8b5201
Jack Coady <jc776@cam.ac.uk>
2014-12-17 19:34:11 +0000

    Part 1 cross-compiling 'hello world'

---
+++ hello_openwrt_package/src/helloworld.c
@@ -0,0 +1,11 @@
+/****************
+* helloworld.c
+* The most simplistic C program ever written.
+*****************/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, OpenWRT package manager!\n\n");
+	return 0;
+}
