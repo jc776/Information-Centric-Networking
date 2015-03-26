51eaa3629068c8c02053a5c2b976cce18799afdd
Jack <jc776@cam.ac.uk>
2015-03-26 15:13:29 +0000

    Same hardware between QoS Single Client examples

--- ns3_scripts/blackadder-jc776/src/QoS/QoS_TCP_single_client.cpp
+++ ns3_scripts/blackadder-jc776/src/QoS/QoS_TCP_single_client.cpp
@@ -34,8 +34,9 @@ int main(int argc, char *argv[]) {
 
    
    // "Hardware" setup
-   // - Same hardware as QoS-Blackadder.
-   // - "import QoS-single-client..."?
+   // - This is the same as the generated one, but usable with the logging tools.
+   // - Common to both QoS Single Client files.
+   // - Still haven't made it a common file.
    NodeContainer nodes;
    nodes.Create (3);
    
@@ -47,6 +48,7 @@ int main(int argc, char *argv[]) {
     *    2   <-- Client, PacketSinkApplication (Recieves from 0)
     */
    
+   // Nodes
    Ptr<Node> node0 = nodes.Get (0);
    Ptr<Node> node1 = nodes.Get (1);
    Ptr<Node> node2 = nodes.Get (2);
@@ -54,17 +56,17 @@ int main(int argc, char *argv[]) {
    NodeContainer n0n1(node0,node1);
    NodeContainer n1n2(node1,node2);
    
-   // These are the same kind of thing, but separated so I can log server/router by itself.
+   // Wire attributes
    PointToPointHelper p2p;
    p2p.SetDeviceAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
    p2p.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (10)));
    p2p.SetDeviceAttribute ("Mtu", UintegerValue (1500));
    
+   // Install wires
    NetDeviceContainer wires_server_router = p2p.Install(n0n1);
    NetDeviceContainer wires_router_client = p2p.Install(n1n2);
    
-   // TCP/IP hardware doesn't need exact MAC addresses
-/*
+   // MAC Addresses (not used by TCP)
    // 0-1
    Ptr<NetDevice> dev0_0 = node0->GetDevice(0); // generated was "PointToPointNetDevice"
    Ptr<NetDevice> dev1_0 = node1->GetDevice(0);
@@ -76,8 +78,6 @@ int main(int argc, char *argv[]) {
    Ptr<NetDevice> dev2_0 = node2->GetDevice(0);
    dev1_1->SetAddress (Mac48Address("7f:f1:57:17:1b:1a"));
    dev2_0->SetAddress (Mac48Address("7f:f1:57:17:1b:1b"));
-*/
-   //NS_LOG_INFO("0->1: " << dev0_0);
    
   // Underlying software setup
   InternetStackHelper internet;
